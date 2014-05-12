e41b6b313268d7884ed563e4d1963f31ffb0acf3
Jerome Cheng <jerome@ayulin.net>
2014-05-12 22:55:13 +0800

    Add Hurricane class.

---
+++ WundergroundClient/Autocomplete/Hurricane.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WundergroundClient.Autocomplete
+{
+    /// <summary>
+    /// Tropical Cyclone Basins used by Wunderground.
+    ///
+    /// Though NorthIndian and SouthIndian are referred to in 
+    /// the "currenthurricane" feature documentation, 
+    /// Autocomplete doesn't appear to return any hurricanes 
+    /// that fall into these basins.
+    /// </summary>
+    enum TropicalCycloneBasin
+    {
+        NorthAtlantic, // "at"
+        EasternPacific, // "ep"
+        WesternPacific, // "wp"
+        NorthIndian, // "ni"
+        SouthIndian // "si"
+    }
+    class Hurricane : AutocompleteResponseObject
+    {
+        /// <summary>
+        /// The date on which this hurricane occurred.
+        /// The time properties of this field should be ignored;
+        /// Wunderground does not provide timing information.
+        /// </summary>
+        public DateTime Date;
+
+        /// <summary>
+        /// The Tropical Cyclone Basin that this hurricane occurred in.
+        /// </summary>
+        public TropicalCycloneBasin Basin;
+    }
+}
