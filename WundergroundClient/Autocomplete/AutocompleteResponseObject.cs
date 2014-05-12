99a703a90b36771feb30aa9cc8ff717ffe255c08
Jerome Cheng <jerome@ayulin.net>
2014-05-12 22:41:18 +0800

    Add AutocompleteResponseObject and City.

---
+++ WundergroundClient/Autocomplete/AutocompleteResponseObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WundergroundClient.Autocomplete
+{
+    abstract class AutocompleteResponseObject
+    {
+        public String Name;
+        public String Link;
+    }
+}
---
+++ WundergroundClient/Autocomplete/City.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WundergroundClient.Autocomplete
+{
+    class City : AutocompleteResponseObject
+    {
+        public String Country;
+
+        /// <summary>
+        /// Corresponds to the "zmw" field used in links.
+        /// </summary>
+        public String Identifier;
+
+        /// <summary>
+        /// The Olson-format time zone.
+        /// e.g. America/Argentina/La_Rioja
+        /// </summary>
+        public String TimeZone;
+
+        /// <summary>
+        /// Abbreviated time zone.
+        /// e.g. ART
+        /// </summary>
+        public String ShortTimeZone;
+
+        // Though the API documentation includes latitude and longitude
+        // coordinates, they do not appear to actually be returned in
+        // the response.
+    }
+}
