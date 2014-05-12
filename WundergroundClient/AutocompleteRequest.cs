918782804db6d87d6e7f14a8ffed0336ada81e57
Jerome Cheng <jerome@ayulin.net>
2014-05-12 20:04:28 +0800

    Add AutocompleteRequest with basic constructors.

---
+++ WundergroundClient/AutocompleteRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WundergroundClient
+{
+    enum ResultFilter
+    {
+        CitiesOnly,
+        HurricanesOnly,
+        CitiesAndHurricanes
+    }
+
+    class AutocompleteRequest
+    {
+        private String _searchString;
+        private ResultFilter _filter = ResultFilter.CitiesOnly;
+        private String _countryCode = null;
+
+        public AutocompleteRequest(String searchString)
+        {
+            _searchString = searchString;
+        }
+
+        public AutocompleteRequest(String query, ResultFilter filter)
+        {
+            _searchString = query;
+            _filter = filter;
+        }
+
+        public AutocompleteRequest(String searchString, ResultFilter filter, string countryCode)
+        {
+            _searchString = searchString;
+            _filter = filter;
+            _countryCode = countryCode;
+        }
+    }
+}
