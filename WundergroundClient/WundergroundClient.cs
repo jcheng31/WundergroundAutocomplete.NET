4770a2ff3c61ae15379679d2f6a0f141a4dceec9
Jerome Cheng <jerome@ayulin.net>
2014-05-12 19:36:12 +0800

    Basic constructor.

--- WundergroundClient/WundergroundClient.cs
+++ WundergroundClient/WundergroundClient.cs
@@ -7,5 +7,13 @@ namespace WundergroundClient
 {
     public class WundergroundClient
     {
+        private const String BaseUrl = "http://api.wunderground.com/api";
+
+        private String _apiKey;
+
+        public WundergroundClient(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
     }
 }
