c057c5349a4691dd90abcb4dcef37fabecbf6168
Jerome Cheng <jerome@ayulin.net>
2014-05-13 14:26:58 +0800

    Add comments saying what each field corresponds to.

--- WundergroundClient/Autocomplete/City.cs
+++ WundergroundClient/Autocomplete/City.cs
@@ -4,31 +4,40 @@ namespace WundergroundClient.Autocomplete
 {
     class City : Result
     {
-        public String Country;
-
         /// <summary>
-        /// Corresponds to the "zmw" field used in links.
+        /// Corresponds to the "zmw" field.
         /// </summary>
         public String Identifier;
 
         /// <summary>
-        /// The Olson-format time zone.
-        /// e.g. America/Argentina/La_Rioja
+        /// The Olson-format time zone (e.g. America/Argentina/La_Rioja)
+        /// 
+        /// Corresponds to the "tz" field.
         /// </summary>
         public String TimeZone;
 
         /// <summary>
-        /// Abbreviated time zone.
-        /// e.g. ART
+        /// Abbreviated time zone (e.g. ART)
+        /// 
+        /// Corresponds to the "tzs" field.
         /// </summary>
         public String ShortTimeZone;
 
+        /// <summary>
+        /// Corresponds to the "lat" field.
+        /// </summary>
         public double Latitude;
+
+        /// <summary>
+        /// Corresponds to the "lon" field.
+        /// </summary>
         public double Longitude;
 
         /// <summary>
         /// The latitude and longitude of this city, separated
         /// by a space.
+        /// 
+        /// Corresponds to the "ll" field.
         /// </summary>
         public String LatitudeLongitude;
 
@@ -38,6 +47,8 @@ namespace WundergroundClient.Autocomplete
         /// Note: Wunderground uses different country codes than
         /// the standard ISO. Check http://www.wunderground.com/weather/api/d/docs?d=resources/country-to-iso-matching
         /// for mapping between this field and the ISO specification.
+        /// 
+        /// Corresponds to the "c" field.
         /// </summary>
         public String CountryCode;
     }
--- WundergroundClient/Autocomplete/Hurricane.cs
+++ WundergroundClient/Autocomplete/Hurricane.cs
@@ -9,6 +9,8 @@ namespace WundergroundClient.Autocomplete
     /// the "currenthurricane" feature documentation, 
     /// Autocomplete doesn't appear to return any hurricanes 
     /// that fall into these basins.
+    /// 
+    /// Corresponds to the "basin" field.
     /// </summary>
     enum TropicalCycloneBasin
     {
@@ -23,16 +25,22 @@ namespace WundergroundClient.Autocomplete
         /// <summary>
         /// The date on which this hurricane occurred. The time properties of this 
         /// field should be ignored.
+        /// 
+        /// Corresponds to the "date" field.
         /// </summary>
         public DateTime Date;
 
         /// <summary>
         /// The Tropical Cyclone Basin that this hurricane occurred in.
+        /// 
+        /// Corresponds to the "basin" field.
         /// </summary>
         public TropicalCycloneBasin Basin;
 
         /// <summary>
         /// The Tropical Depression number of this hurricane.
+        /// 
+        /// Corresponds to the "strmnum" field.
         /// </summary>
         public int StormNumber;
 
@@ -41,17 +49,36 @@ namespace WundergroundClient.Autocomplete
         ///
         /// Note: this value is sometimes unknown. In that case, this field
         /// will be -1.
+        /// 
+        /// Corresponds to the "damage" field.
         /// </summary>
         public int Damage;
 
         /// <summary>
         /// The highest category this hurricane reached on the Saffir-Simpson scale.
+        /// 
+        /// Corresponds to the "maxcat" field.
         /// </summary>
         public int PeakCategory;
 
+        /// <summary>
+        /// Corresponds to the "sw_lat" field.
+        /// </summary>
         public double SouthWestLatitude;
+
+        /// <summary>
+        /// Corresponds to the "sw_lon" field.
+        /// </summary>
         public double SouthWestLongitude;
+
+        /// <summary>
+        /// Corresponds to the "ne_lat" field.
+        /// </summary>
         public double NorthEastLatitude;
+
+        /// <summary>
+        /// Corresponds to the "ne_lon" field.
+        /// </summary>
         public double NorthEastLongitude;
 
         /// <summary>
@@ -60,6 +87,8 @@ namespace WundergroundClient.Autocomplete
         /// 
         /// Note: this date is sometimes unknown. In this case, FormationDate
         /// and DissipationDate will be equal to DateTime.MinValue.
+        /// 
+        /// Corresponds to the "start_epoch" field.
         /// </summary>
         public DateTime FormationDate;
 
@@ -69,6 +98,8 @@ namespace WundergroundClient.Autocomplete
         ///
         /// Note: this date is sometimes unknown. In this case, FormationDate
         /// and DissipationDate will be equal to DateTime.MinValue.
+        /// 
+        /// Corresponds to the "end_epoch" field.
         /// </summary>
         public DateTime DissipationDate;
     }
