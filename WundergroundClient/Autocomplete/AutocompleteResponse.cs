1d2e51e31701990d98b301ffd2caab4740a63e5f
Jerome Cheng <jerome@ayulin.net>
2014-05-13 10:42:33 +0800

    Add skeleton for Autocomplete Responses.

---
+++ WundergroundClient/Autocomplete/AutocompleteResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WundergroundClient.Autocomplete
+{
+    class AutocompleteResponse
+    {
+        public Boolean IsSuccessful { get; private set; }
+        public IList<AutocompleteResponseObject> Results;
+    }
+}
